55f7c9304a3ea7cd394911f4f39a82c8c5cc42f4
Jim Finn <jim.finnjim2001@gmail.com>
2023-01-20 13:49:07 +0000

    21 and 22 complete

---
+++ 20-26/21.py
@@ -0,0 +1,5 @@
+firstName = input("What is your first name? ")
+lastName = input("What is your last Name? ")
+name = firstName +" "+lastName
+length = len(name)
+print("Hello",name,"Your name is",length,"Characters long")
---
+++ 20-26/22.py
@@ -0,0 +1,5 @@
+firstName = input("Enter your first name in lower case ")
+lastName = input("Enter your last name in lower Case ")
+name = firstName + " " + lastName
+titles = name.title()
+print("You entered",name,"This in title case looks like",titles)
